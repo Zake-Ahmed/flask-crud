1eabf1524ca50612e22662945536c143f8671029
Zake-Ahmed <zake3012@gmail.com>
2022-06-14 15:01:16 +0000

    model update

--- application/routes.py
+++ application/routes.py
@@ -1,35 +1,35 @@
 from application import app, db
-from application.models import Users,Posts
-@app.route('/add/<user>')
-@app.route('/add/<user>/<first>/<last>')
-def add(user,first="null",last="null"):
-    new_user = Users(firstName=first,lastName=last,userName=user)
-    db.session.add(new_user)
+from application.models import UserUsers,Posts
+@app.route('/add/<User>')
+@app.route('/add/<User>/<first>/<last>')
+def add(User,first="null",last="null"):
+    new_User = UserUsers(firstName=first,lastName=last,UserName=User)
+    db.session.add(new_User)
     db.session.commit()
-    return f"Added {user} to database"
+    return f"Added {User} to database"
 
 @app.route('/read')
 def read():
-    all_Users = Users.query.all()
-    Users_string = ""
-    for user in all_Users:
-        Users_string += "<br>"+  "ID:"+str(user.id) + "||First Name:" + user.firstName + "||Last Name:" + user.lastName + "||User Name:" + user.userName 
-    return Users_string
+    all_UserUsers = UserUsers.querUsery.all()
+    UserUsers_string = ""
+    for User in all_UserUsers:
+        UserUsers_string += "<br>"+  "ID:"+str(User.id) + "||First Name:" + User.firstName + "||Last Name:" + User.lastName + "||UserUser Name:" + User.UserName 
+    return UserUsers_string
 
-@app.route('/update/<int:id>/<user>/<first>/<last>')
-def update(id,user,first,last):
-    first_user = Users.query.get(id)
-    first_user.firstName = first
-    first_user.lastName = last
-    first_user.userName = user
+@app.route('/update/<int:id>/<User>/<first>/<last>')
+def update(id,User,first,last):
+    first_User = UserUsers.querUsery.get(id)
+    first_User.firstName = first
+    first_User.lastName = last
+    first_User.UserName = User
     db.session.commit()
-    return f"Updated user id {id}"
+    return f"Updated User id {id}"
 
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    user = Users.query.get(id)
-    db.session.delete(user)
+    User = UserUsers.querUsery.get(id)
+    db.session.delete(User)
     db.session.commit()
-    return f"Deleted user id {id}"
+    return f"Deleted User id {id}"
 
