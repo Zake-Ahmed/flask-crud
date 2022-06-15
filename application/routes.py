bb1789a8ada75873c130b45455a1deb5ff0661ae
Zake-Ahmed <zake3012@gmail.com>
2022-06-15 10:40:49 +0000

    commit challenge

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from application.models import Users,Posts
 @app.route('/add/<User>')
 @app.route('/add/<User>/<first>/<last>')
 def add(User,first="null",last="null"):
-    new_User = Users(firstName=first,lastName=last,UserName=User)
+    new_User = Users(firstName=first,lastName=last,userName=User)
     db.session.add(new_User)
     db.session.commit()
     return f"Added {User} to database"
@@ -13,15 +13,15 @@ def read():
     all_Users = Users.query.all()
     Users_string = ""
     for User in all_Users:
-        Users_string += "<br>"+  "ID:"+str(User.id) + "||First Name:" + User.firstName + "||Last Name:" + User.lastName + "||UserUser Name:" + User.UserName 
+        Users_string += "<br>"+  "ID:"+str(User.id) + "||First Name:" + User.firstName + "||Last Name:" + User.lastName + "||User Name:" + User.userName 
     return Users_string
 
 @app.route('/update/<int:id>/<User>/<first>/<last>')
 def update(id,User,first,last):
-    first_User = UserUsers.query.get(id)
+    first_User = Users.query.get(id)
     first_User.firstName = first
     first_User.lastName = last
-    first_User.UserName = User
+    first_User.userName = User
     db.session.commit()
     return f"Updated User id {id}"
 
@@ -40,3 +40,18 @@ def count():
     for User in all_Users:
         count+=1
     return f'The number of users in db is {count}'
+
+@app.route('/updatename/<oldUser>/<newUser>')
+def updatename(oldUser,newUser):
+    first_User = Users.query.filter_by(userName=oldUser).first()
+    first_User.userName = newUser
+    db.session.commit()
+    return f"Updated User {oldUser} to {newUser}"
+
+@app.route('/deletename/<User>')
+def deletename(User):
+    User = Users.query.filter_by(userName=User).first()
+    db.session.delete(User)
+    db.session.commit()
+    return f"Deleted User {User}"
+
